8aaf244b63b706f774b5edbaa0e96c3cc070dcd7
prince_geo <prince.geovision.tw@gmail.com>
2022-06-15 15:13:24 +0800

    add version to Frameworks

--- Sources/APHelperKit/APHelperKit.swift
+++ Sources/APHelperKit/APHelperKit.swift
@@ -1,3 +1,9 @@
-struct APHelperKit {
+public struct APHelperKit {
+    //default from creation.
     var text = "Hello, World!"
+    //version
+    public let version = "v0.1.0"
+    
+    public init() { }
+    
 }
